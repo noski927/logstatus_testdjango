e45fe775986132bebd415bb231bf5d7aa025eb06
George Dm <go.dmit@mail.ru>
2025-09-01 03:23:31 +0300

    refactor logs and views: reduce log fetch interval to 1 second and redirect after 3 seconds

--- pages/urls.py
+++ pages/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("start/", views.start_task, name="start_task"),
-    path("logs/task/", views.logs_view, name="logs"),
-    path("logs/data/", views.get_logs, name="get_logs"),
+    path("logs/task/", views.logs_view, name="logs"),  # Отображение логов выполнения задачи
+    path("logs/data/", views.get_logs, name="get_logs"),  # Получение логов выполнения задачи
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,30 +1,41 @@
+import threading
 import time
 
+from django.http import HttpRequest, JsonResponse
 from django.shortcuts import redirect, render
 
 TASK_LOGS = []
 TASK_RUNNING = False
 
 
-def long_process(num_seconds: int):
+def long_process(num_seconds: int) -> None:
+    """
+    Функция имитирующая длительную задачу
+    """
     global TASK_LOGS, TASK_RUNNING
     TASK_LOGS = []
     TASK_RUNNING = True
-    for i in range(num_seconds):
+    for i in range(num_seconds):  # имитация длительной задачи
         msg = f"Running for {i + 1} seconds..."
-        print(msg)  # видно в консоли
+        print(msg)
         TASK_LOGS.append(msg)  # сохраняем в память для фронта
-        time.sleep(1)
+        time.sleep(1)  # интервал в работе
     TASK_LOGS.append("Задача завершена ✅")
     TASK_RUNNING = False
 
 
-def index(request):
+def index(request: HttpRequest) -> render:
+    """
+    Отображает основную страницу приложения с формой
+    """
     status = request.GET.get("status")
     return render(request, "pages/index.html", {"status": status})
 
 
-def start_task(request):
+def start_task(request: HttpRequest) -> redirect:
+    """
+    Запуск длительной задачи в отдельном потоке
+    """
     if request.method == "POST":
         seconds = int(request.POST.get("seconds", 5))
         thread = threading.Thread(target=long_process, args=(seconds,))
@@ -33,9 +44,15 @@ def start_task(request):
     return redirect("index")
 
 
-def logs_view(request):
+def logs_view(request: HttpRequest) -> render:
+    """
+    Отображение логов выполнения задачи
+    """
     return render(request, "pages/logs.html")
 
 
-def get_logs(request):
+def get_logs(request: HttpRequest) -> JsonResponse:
+    """
+    Получение логов выполнения задачи
+    """
     return JsonResponse({"logs": TASK_LOGS, "running": TASK_RUNNING})
